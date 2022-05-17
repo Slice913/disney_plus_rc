40c31eb57093e1a824bea00e1235892694f5d2dc
Slice913 <akeemayoade@gmail.com>
2022-05-17 14:29:40 -0500

    ADD: Detail page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Detail from './components/Detail';
 import Home from './components/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
-      {/* <Viewer> */}
-      {/* MoviesList */}
+      <Router>
+        <Header />
+        <Routes>
+            <Route path="/detail" element={<Detail/>}/>
+            <Route path="/" element={<Home/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Detail.js
@@ -0,0 +1,124 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Detail() {
+  return (
+    <Container>
+        <Background>
+            <img src="https://images.trutv.com/tru/$dyna_params/https%3A%2F%2Fi.cdn.trutv.com%2Fassets%2Ftru%2Fprod%2Fimages%2F2021%2F01%2FRogueOne-AStarWarsStory-1600x900.jpg" />
+        </Background>
+        <ImgTitle>
+            <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/679119E881B53C0ED9485B0A245963AB9E428F65A165908BC64C1AFC46C4D8C6/scale?width=1200&aspectRatio=1.78" />
+        </ImgTitle>
+        <Controls>
+            <PlayButton>
+                <img src="/DisImages/play-icon-black.png" />
+                <span>PLAY</span>
+            </PlayButton>
+            <TrailerButton>
+                <img src="/DisImages/play-icon-white.png" />
+                <span>Trailer</span>
+            </TrailerButton>
+            <AddButton>
+                <span>+</span>
+            </AddButton>
+            <GroupWatchButton>
+                <img src="/DisImages/group-icon.png" />
+            </GroupWatchButton>
+
+        </Controls>
+    </Container>
+  )
+}
+
+export default Detail
+
+const Container = styled.div`
+    min-height:  calc(100vh - 70px);
+    padding: 0 calc(3.5vw + 5px);
+    position: relative;
+`
+
+const Background = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0; 
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+    opacity: 0.8;
+
+    img {
+        width: 100%;
+        height: 100%
+        object-fit: cover;
+    }
+`
+
+const ImgTitle = styled.div`
+    height: 30vh;
+    min-height: 170px;
+    width: 25vw;
+    min-width: 200px;
+
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+`
+
+const Controls = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const PlayButton = styled.button`
+    border-radius: 4px;
+    font-size: 15px;
+    display: flex;
+    align-items: center;
+    height: 56px;
+    background: rgb (249, 249, 249);
+    border: none;
+    padding: 0px  24px;
+    margin-right: 22px;
+    letter-spacing: 1.8px;
+    cursor: pointer;
+
+    &:hover {
+        background: rgb(198, 198, 198);
+    }
+`
+
+
+const TrailerButton = styled(PlayButton)`
+    background: rgba(0, 0, 0, 0.3);
+    border: 1px solid rgb(249, 249, 249);
+    color: rgb(249, 249, 249);
+    text-transform: uppercase;
+
+
+`
+
+const AddButton = styled.button`
+    margin-right: 16px;
+    width: 44px;
+    height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    border: 2px solid white;
+    background-color:rgba(0, 0, 0, 0.6);
+    cursor: pointer;
+
+    span {
+        font-size: 30px;
+        color: white;
+    }
+
+`
+
+const GroupWatchButton = styled(AddButton)``
